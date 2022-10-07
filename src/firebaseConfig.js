b9b1ac43ffe58ac9235fbe24dcfeab94b08eaaef
BonganiBmG <m.bongani@icloud.com>
2022-10-07 08:44:56 +0200

    Updated V.3 with firebaseConfig

---
+++ src/firebaseConfig.js
@@ -0,0 +1,13 @@
+import * as firebase from firebase;
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBtKYtofqTi6v9auZOJDih_e7-8pddvW1Q",
+    authDomain: "myhcp-310f9.firebaseapp.com",
+    projectId: "myhcp-310f9",
+    storageBucket: "myhcp-310f9.appspot.com",
+    messagingSenderId: "63814691379",
+    appId: "1:63814691379:web:873cf9058508e8a2d95146",
+    measurementId: "G-5QBZ9QZP0K"
+  };
+
+export default (firebaseConfig); 
